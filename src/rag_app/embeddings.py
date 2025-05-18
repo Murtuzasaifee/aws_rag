9808878a853b8ef76e25f6364f02e7029e4733a7
Murtuza Saifee <msaifee@firstorion.com>
2025-05-18 14:03:58 +0400

    Updating code with the docker approach only

--- src/rag_app/embeddings.py
+++ src/rag_app/embeddings.py
@@ -1,6 +1,16 @@
 from langchain_aws import BedrockEmbeddings
+import os
+import boto3
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def get_embedding():
-    embeddings = BedrockEmbeddings()
+    region = os.environ.get("AWS_DEFAULT_REGION", "us-east-1").strip()
+
+    session = boto3.Session(region_name=region, aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID"),
+                            aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY"))
+
+
+    embeddings = BedrockEmbeddings(client=session.client('bedrock-runtime'), model_id="amazon.titan-embed-text-v1")
     return embeddings
