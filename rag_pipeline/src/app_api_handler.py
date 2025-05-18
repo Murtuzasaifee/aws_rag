7c503741168fbe0695f12eca24209e87d68969fa
Murtuza Saifee <msaifee@firstorion.com>
2025-05-18 16:00:44 +0400

    Implmentating the feature of inferencing with dynamo in between to combat 15 mins lambda limit

--- rag_pipeline/src/app_api_handler.py
+++ rag_pipeline/src/app_api_handler.py
@@ -2,7 +2,8 @@ import uvicorn
 from fastapi import FastAPI
 from mangum import Mangum
 from pydantic import BaseModel
-from src.rag_app.query_rag import QueryResponse, query_rag
+from src.query_model import QueryModel
+from src.rag_app.query_rag import query_rag
 
 app = FastAPI()
 handler = Mangum(app)  # Entry point for AWS Lambda.
@@ -17,10 +18,26 @@ def index():
     return {"Hello": "World"}
 
 
+@app.get("/get_query")
+def get_query_endpoint(query_id: str) -> QueryModel:
+    query = QueryModel.get_item(query_id)
+    return query
+
+
 @app.post("/submit_query")
-def submit_query_endpoint(request: SubmitQueryRequest) -> QueryResponse:
+def submit_query_endpoint(request: SubmitQueryRequest) -> QueryModel:
+
     query_response = query_rag(request.query_text)
-    return query_response
+
+    # Create the query item, and put it into the data-base.
+    new_query = QueryModel(
+        query_text=request.query_text,
+        answer_text=query_response.response_text,
+        sources=query_response.sources,
+        is_complete=True,
+    )
+    new_query.put_item()
+    return new_query
 
 
 if __name__ == "__main__":
@@ -28,3 +45,4 @@ if __name__ == "__main__":
     port = 8000
     print(f"Running the FastAPI server on port {port}.")
     uvicorn.run("app_api_handler:app", host="0.0.0.0", port=port)
+            
