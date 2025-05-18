01e3db7fd5753210c5ca3b493b9d651bbb3e335c
Murtuza Saifee <msaifee@firstorion.com>
2025-05-18 16:18:49 +0400

    Creating dynamo stack

--- aws-rag-infra/aws_rag_infra/aws_rag_infra_stack.py
+++ aws-rag-infra/aws_rag_infra/aws_rag_infra_stack.py
@@ -2,17 +2,27 @@ from aws_cdk import (
     Stack,
     Duration,
     CfnOutput,
+    aws_dynamodb as dynamodb,
     aws_lambda as lambda_,
     aws_iam as iam
 )
 from constructs import Construct
 
-class AwsRagInfraStack(Stack):
-
+class RagCdkInfraStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-       # Function to handle the API requests
+        # Create a DynamoDB table to store the query data and results
+        rag_query_table = dynamodb.Table(
+            self, "RagQueryTable",
+            partition_key=dynamodb.Attribute(
+                name="query_id", 
+                type=dynamodb.AttributeType.STRING
+            ),
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST
+        )
+
+        # Function to handle the API requests
         api_image_code = lambda_.DockerImageCode.from_image_asset(
             "../rag_pipeline",
             cmd=["src/app_api_handler.handler"],
@@ -25,8 +35,11 @@ class AwsRagInfraStack(Stack):
             self, "ApiFunc",
             code=api_image_code,
             memory_size=256,
-            timeout=Duration.seconds(300),
-            architecture=lambda_.Architecture.X86_64
+            timeout=Duration.seconds(30),
+            architecture=lambda_.Architecture.X86_64,
+            environment={
+                "TABLE_NAME": rag_query_table.table_name
+            }
         )
 
         # Public URL for the API function
@@ -34,7 +47,8 @@ class AwsRagInfraStack(Stack):
             auth_type=lambda_.FunctionUrlAuthType.NONE
         )
 
-        # Grant permissions
+        # Grant permissions for all resources to work together
+        rag_query_table.grant_read_write_data(api_function)
         api_function.role.add_managed_policy(
             iam.ManagedPolicy.from_aws_managed_policy_name("AmazonBedrockFullAccess")
         )
